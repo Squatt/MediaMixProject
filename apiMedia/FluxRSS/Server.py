b883b416dde5356fc9ec0ef8ecc4713cede7d44c
Luka Peschke <mail@lukapeschke.com>
2017-09-10 17:22:58 +0200

    Dummy function on /getActualities

--- apiMedia/FluxRSS/Server.py
+++ apiMedia/FluxRSS/Server.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
 
+import json
+
 from apiMedia.FluxRSS.imports.Server_IMPORTS import *
 from apiMedia.FluxRSS.FluxRSSManager import *
-
+from apiMedia.RSSParser import LeMondeParser, MediaPartParser
+from apiMedia.utils import get_nouns
 
 app = Flask(__name__)
 
 
+parsers = [
+    LeMondeParser,
+    MediaPartParser,
+]
+
+def get_titles():
+    titles = []
+    for parser in parsers:
+        titles.extend(parser().get_titles())
+    text = ' '.join(titles)
+    return json.dumps(titles) + json.dumps(get_nouns(text))
+
 ## Route flux RSS Manager ##
-app.add_url_rule('/getActualities', 'getActualities', GetActualities, methods=['GET'])
+app.add_url_rule('/getActualities', 'getActualities',
+                 get_titles, methods=['GET'])
 
 
 def start():
